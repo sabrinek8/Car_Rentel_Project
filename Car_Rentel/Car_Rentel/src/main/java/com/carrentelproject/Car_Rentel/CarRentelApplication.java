bc07e1961ceb14b300fab719eeaf74dc6eec9802
Sabrine Kammoun <sabrinkammoun@gmail.com>
2024-02-09 00:41:40 +0100

    this is commit

---
+++ Car_Rentel/Car_Rentel/src/main/java/com/carrentelproject/Car_Rentel/CarRentelApplication.java
@@ -0,0 +1,13 @@
+package com.carrentelproject.Car_Rentel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentelApplication.class, args);
+	}
+
+}
---
+++ Car_Rentel/Car_Rentel/src/test/java/com/carrentelproject/Car_Rentel/CarRentelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrentelproject.Car_Rentel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
