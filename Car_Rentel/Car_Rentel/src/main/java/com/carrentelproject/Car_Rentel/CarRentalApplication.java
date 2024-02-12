73116a5b22db8083c44b88af0c5d2e1f54b489b6
Sabrine Kammoun <sabrinkammoun@gmail.com>
2024-02-12 22:01:24 +0100

    Angular added

--- Car_Rentel/Car_Rentel/src/main/java/com/carrentelproject/Car_Rentel/CarRentalApplication.java
+++ Car_Rentel/Car_Rentel/src/main/java/com/carrentelproject/Car_Rentel/CarRentalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CarRentelApplication {
+public class CarRentalApplication{
 
 	public static void main(String[] args) {
-		SpringApplication.run(CarRentelApplication.class, args);
+		SpringApplication.run(CarRentalApplication.class, args);
 	}
 
 }
